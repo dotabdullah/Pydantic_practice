4163098c1067160d561e0c9243179dcd1924bfbc
Abdullah Shahzad <a3tavengers@gmail.com>
2026-09-18 16:18:39 +0500

    Initial commit: Pydantic Practice

---
+++ pydantic_why.py
@@ -0,0 +1,18 @@
+# Usually python allows dynamic typing not static typing like Java or C++.
+# Python type hunting is a way to add type hints to your code not producing errors. 
+# Pythont does not have Type Validation by default.
+
+def insert_patient_data(name: str, age: int):
+    
+    if type(name) == str and type(age) == int:
+        if age < 0:
+            raise ValueError("Age cann't be negative, please add correct age.")
+        else:     
+            print(name)
+            print(age)
+            print("inserted into database")
+    else:
+        raise TypeError("Incorrect data type.")    
+
+
+insert_patient_data("Abdullah", 30)    
